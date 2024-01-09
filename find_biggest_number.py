1feb6a28b487787d5415809dc06860a9908260dc
veronbungag <rveron.bungag@gmail.com>
2024-01-10 00:47:31 +0800

    pseudocode

---
+++ find_biggest_number.py
@@ -0,0 +1,9 @@
+#ask user to input first number and store in variable first number
+#ask user to input second number and store in variable second number
+#ask user to input third number store in variable third number
+#if first number is greater than second number
+#if first number is greater than third number
+#else store third number is biggest number
+#else 
+#if second number is greater than third number
+#else third number is biggest number

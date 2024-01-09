55cd2bee5a691f93c74637827f43891bf14d5c31
veronbungag <rveron.bungag@gmail.com>
2024-01-10 00:59:35 +0800

    if-else

--- find_biggest_number.py
+++ find_biggest_number.py
@@ -5,8 +5,12 @@ second_number = int(input("Enter second number:"))
 #ask user to input third number store in variable third number
 third_number = int(input("Enter third number:"))
 #if first number is greater than second number
+if first_number > second_number:
 #if first number is greater than third number
+ if first_number > third_number:
 #else store third number is biggest number
+else: 
+     biggest_number = third_number
 #else 
 #if second number is greater than third number
 #else third number is biggest number

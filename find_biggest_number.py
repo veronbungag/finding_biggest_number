b8f02c46255d120860950d1fa1295be4840ddde8
veronbungag <rveron.bungag@gmail.com>
2024-01-10 01:14:06 +0800

    updated code for function and if-else statement

--- find_biggest_number.py
+++ find_biggest_number.py
@@ -1,16 +1,19 @@
-#ask user to input first number and store in variable first number
-first_number = int(input("Enter first number:"))
-#ask user to input second number and store in variable second number
-second_number = int(input("Enter second number:"))
-#ask user to input third number store in variable third number
-third_number = int(input("Enter third number:"))
-#if first number is greater than second number
-if first_number > second_number:
-#if first number is greater than third number
- if first_number > third_number:
-#else store third number is biggest number
-else: 
-     biggest_number = third_number
+#define find biggest number
+#ask user to input three numbers and store in each variables
+
+def find_biggest():
+ first = int(input("Enter first number:"))
+ second = int(input("Enter second number:"))
+ third = int(input("Enter third number:"))
+ 
+ #if first number is greater than second number
+ if first > second:
+      #if first number is greater than third number
+        if first > third:
+            biggest = first
+      #else store third number is biggest number
+        else:
+            biggest = third
 #else 
 #if second number is greater than third number
 #else third number is biggest number

f8d1ca268e38c7c5d3adc04eaad3dad258c4a53a
veronbungag <rveron.bungag@gmail.com>
2024-01-10 11:32:27 +0800

    updated code in if else for working window root

--- find_biggest_number.py
+++ find_biggest_number.py
@@ -1,32 +1,31 @@
 #import tkinter interface
-import tkinter as tk
-from tkinter import messagebox
+from tkinter import *
 
 #define find biggest number
 #ask user to input three numbers and store in each variables
 
 def find_biggest():
- first = int(input("Enter first number:"))
- second = int(input("Enter second number:"))
- third = int(input("Enter third number:"))
+     first = int(first.get())
+     second = int(second.get())
+     third = int(third.get())
  
  #if first number is greater than second number
- if first > second:
+     if first > second:
       #if first number is greater than third number
         if first > third:
             biggest = first
       #else store third number is biggest number
         else:
             biggest = third
- else:
+     else:
       #if second number is greater than third number
         if second > third:
             biggest = second
       #else third number is biggest number
         else:
             biggest = third 
- #print data
- print("The biggest number is: ", biggest)
+     result_label.config(text="The biggest number is: " + str(biggest))
+            
 
 find_biggest()
 
@@ -39,7 +38,6 @@ root.config(background = "pink")
 frame = Frame(root)
 frame.pack()
 #Display a dialog box asking the user to enter the first number
-
 #Display a dialog box asking the user to enter the second number
 #Display a dialog box asking the user to enter the third number
 #add button
